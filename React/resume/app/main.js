35d5826d22b9c3237829d56cad971cc31702f2fd
xiaoxiongmila <540664191@qq.com>
2016-05-31 17:51:21 +0800

    Add entry file

---
+++ React/resume/app/main.js
@@ -0,0 +1,30 @@
+﻿'use strict'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Logo from './components/logo';
+import Nav from  './components/nav';
+import Navbar from './components/navbar';
+import Row from './components/row';
+import Header from './components/header';
+import DataPanel from './components/datapanel';
+import SkillPanel from './components/skill';
+import Footer from './components/footer';
+import PageContent from './components/pageContent';
+
+
+//class Hello extends React.Component {
+//  render() {
+//    return <div>Hello, likang!!</div>;
+//  }
+//}
+//
+//ReactDOM.render(
+//    <Hello />,
+//    document.getElementById('app')
+//);
+
+//import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
+ReactDOM.render(
+    <PageContent />,
+    document.getElementById('app'));
